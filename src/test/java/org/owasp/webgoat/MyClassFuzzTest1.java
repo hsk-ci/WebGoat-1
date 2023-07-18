216d43542ba1a30bcb966bce015f5a06adbd3cbf
mlsandner <maren.sandner@code-intelligence.com>
2023-07-18 16:47:54 +0200

    included cifuzz in pom with working jacoco

---
+++ src/test/java/org/owasp/webgoat/MyClassFuzzTest1.java
@@ -0,0 +1,15 @@
+package org.owasp.webgoat;
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.code_intelligence.jazzer.junit.FuzzTest;
+
+class MyClassFuzzTest1 {
+    @FuzzTest
+    void myFuzzTest(FuzzedDataProvider data) {
+        int i = data.consumeInt();
+        if (i == 2) {
+            String[] arr = new String[i];
+            System.out.println(arr[i]);
+        }
+    }
+}
