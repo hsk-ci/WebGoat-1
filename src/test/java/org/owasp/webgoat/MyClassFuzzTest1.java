c09127e51a8fbdadf469ab911378c792cbffc30f
Hannes Schenck <hannes.schenck@code-intelligence.com>
2023-07-19 09:54:11 +0200

    [style] fix test style

--- src/test/java/org/owasp/webgoat/MyClassFuzzTest1.java
+++ src/test/java/org/owasp/webgoat/MyClassFuzzTest1.java
@@ -4,12 +4,12 @@ import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import com.code_intelligence.jazzer.junit.FuzzTest;
 
 class MyClassFuzzTest1 {
-    @FuzzTest
-    void myFuzzTest(FuzzedDataProvider data) {
-        int i = data.consumeInt();
-        if (i == 2) {
-            String[] arr = new String[i];
-            System.out.println(arr[i]);
-        }
+  @FuzzTest
+  void myFuzzTest(FuzzedDataProvider data) {
+    int i = data.consumeInt();
+    if (i == 2) {
+      String[] arr = new String[i];
+      System.out.println(arr[i]);
     }
+  }
 }
